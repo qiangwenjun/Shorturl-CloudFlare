c036c7b839df5a0cb7153e60f2c309189966540e
akari <akari@akari.tv>
2026-01-27 13:29:08 +0800

    Remove React app and associated dependencies

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,5 +2,6 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
-
+app.get("/",(c)=> c.redirect("/web/"));
+app.get("/aaa", (c) => c.json({ message: "all" }));
 export default app;
